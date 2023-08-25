8588330d2836912b9f18d8c0f565a9d271b72150
natanael.apaula <natanael.apaula@CAR0588301W10-1>
2023-08-25 16:34:44 -0300

    Adicionar arquivos de projeto.

---
+++ FormBugado/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace FormBugado
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBota = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnBota
+            // 
+            this.btnBota.Location = new System.Drawing.Point(79, 70);
+            this.btnBota.Name = "btnBota";
+            this.btnBota.Size = new System.Drawing.Size(75, 23);
+            this.btnBota.TabIndex = 0;
+            this.btnBota.Text = "Bota";
+            this.btnBota.UseVisualStyleBackColor = true;
+            this.btnBota.Click += new System.EventHandler(this.btnBota_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.Chocolate;
+            this.panel1.Location = new System.Drawing.Point(246, 70);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(231, 0);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnBota);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnBota;
+        private Panel panel1;
+    }
+}
---
+++ FormBugado/Form1.cs
@@ -0,0 +1,62 @@
+namespace FormBugado
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> produtos = new List<string> {"Chinela","Relógio","Basoura","Mortandela" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void geraForm(string produto )
+        {
+
+            Panel panel= new Panel();
+            panel.Name = "panel";
+            panel.BackColor = Color.White;
+            panel.Size = new Size(100,100);
+
+            Label label= new Label();
+            //label.Name=...
+            label.Text = produto;
+            label.AutoSize= true;
+            label.Location = new Point(0,0);
+
+            void comprar(object sender, EventArgs e)
+            {
+                label.Text = "COMPRADO";
+                panel.BackColor = Color.Green;
+            }
+
+            Button button= new Button();
+            button.Text = "COMPRAR";
+            button.Location=new Point(0,label.Height);
+            button.Click += comprar;
+
+            panel.Controls.Add(label);
+            panel.Controls.Add(button); 
+
+            panel.Location=new Point(0,panel1.Height);
+
+            if(panel1.Height<500)
+                panel1.Height += panel.Height+10;
+
+
+            panel1.Controls.Add(panel);
+        }
+
+        private void btnBota_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(string produto in produtos)
+            {
+                geraForm(produto);
+            }
+        }
+    }
+}
